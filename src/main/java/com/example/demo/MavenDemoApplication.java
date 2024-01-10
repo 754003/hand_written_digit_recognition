0b7841e5a73a7d63989f56c68cd5adc6ed82807f
chaitanya <Daggupati.Chaithanya@lumen.com>
2024-01-10 12:48:36 +0530

    inial commit

---
+++ src/main/java/com/example/demo/MavenDemoApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.SQLOutput;
+
+@SpringBootApplication
+public class MavenDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenDemoApplication.class, args);
+	}
+
+}
